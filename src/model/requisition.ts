a1804aa726482a55c9a16050b19473b9a19d0499
Rodger Ruan <rodger.ruan@udtrucks.se>
2024-11-21 13:27:55 +0800

    Requisition model updated

--- src/model/requisition.ts
+++ src/model/requisition.ts
@@ -1,24 +1,24 @@
 export interface IRequisitionGrid {
-  ID?: string;
-  UniqueIdentifier?: string;
+  ID: string;
+  UniqueIdentifier: string;
   IsSelected?: boolean;
-  RequisitionType?: string;
-  Section?: string;
-  Status?: string;
-  PartNumber?: string;
-  Qualifier?: string;
-  MaterialUser?: string;
+  RequisitionType: string;
+  Section: string;
+  Status: string;
+  PartNumber: string;
+  Qualifier: string;
+  MaterialUser: string;
   Project?: string;
   RequiredWeek?: string;
   CreateDate?: Date;
   RfqNo?: string;
   Parma?: string;
-  PartDescription?: string;
+  PartDescription: string;
   AnnualQty?: number;
   OrderQty?: number;
-  ReqBuyer?: string;
-  Handler?: string;
-  HandlerName?: string;
-  BuyerFullInfo?: string;
-  SectionDescription?: string;
+  ReqBuyer: string;
+  Handler: string;
+  HandlerName: string;
+  BuyerFullInfo: string;
+  SectionDescription: string;
 }
