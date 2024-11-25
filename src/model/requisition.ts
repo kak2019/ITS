0c4849ef92cebff57efe588a45b12f2fc494c763
Rodger Ruan <rodger.ruan@udtrucks.se>
2024-11-25 10:13:51 +0800

    refactor: comments added to requisition and rfq model

--- src/model/requisition.ts
+++ src/model/requisition.ts
@@ -1,25 +1,25 @@
 export interface IRequisitionGrid {
-  ID: string;
-  UniqueIdentifier: string;
-  IsSelected?: boolean;
-  RequisitionType: string;
-  Section: string;
-  Status: string;
-  PartNumber: string;
-  Qualifier: string;
-  MaterialUser: string;
-  Project?: string;
-  RequiredWeek?: string;
-  CreateDate?: Date;
-  RfqNo?: string;
-  Parma?: string;
-  PartDescription: string;
-  AnnualQty?: number;
-  OrderQty?: number;
-  ReqBuyer: string;
-  Handler: string;
-  HandlerName: string;
-  BuyerFullInfo: string;
-  SectionDescription: string;
-  Porg:string;
+  ID: string; //SharePoint Item Id
+  UniqueIdentifier: string; //Column "Title"
+  IsSelected?: boolean; // For Grid View, No Corresponding SharePoint List Column
+  RequisitionType: string; //Column "Requisition Type"
+  Section: string; //Column "Section"
+  Status: string; //Column "Status"
+  PartNumber: string; //Column "Part Number"
+  Qualifier: string; //Column "Qualifier"
+  MaterialUser: string; //Column "Material User"
+  Project?: string; //Column "Pproject"
+  RequiredWeek?: string; //Column "Required Week"
+  CreateDate?: Date; //Column "Create Date"
+  RfqNo?: string; //Column "RFQ Number"
+  Parma?: string; //Column Parma
+  PartDescription: string; //Column "Part Description"
+  AnnualQty?: number; //Column "Annual Qty"
+  OrderQty?: number; //Column "Order Qty"
+  ReqBuyer: string; //Column "Requisition Buyer"
+  Handler: string; //Column "Handler"
+  HandlerName: string; //Column "Handler Name"
+  BuyerFullInfo: string; //Column "Buyer Full Info"
+  SectionDescription: string; //Column "Section Description"
+  Porg: string; //Column "Porg"
 }
--- src/model/rfq.ts
+++ src/model/rfq.ts
@@ -1,25 +1,25 @@
 import { IRequisitionGrid } from "./requisition";
 
 export interface IRFQGrid {
-  ID?: string;
-  Title?: string;
-  Parma?: string;
-  SupplierContact?: string;
-  RFQDueDate?: Date;
-  OrderType?: string;
-  RFQInstructionToSupplier?: string;
-  RFQStatus?: string;
-  BuyerInfo?: string;
-  SectionInfo?: string;
-  CommentHistory?: string;
-  RequisitionIds?: string;
-  QuoteReceivedDate?: Date;
-  ReasonOfRFQ?: string;
-  EffectiveDateRequest?: Date;
-  HandlerName?: string;
-  RFQNo?: string;
-  Created?: Date;
-  RFQType?: string;
+  ID?: string; //SharePoint Item Id
+  Title?: string; //Column "Title"
+  Parma?: string; //Column "Parma"
+  SupplierContact?: string; //Column "Supplier Contact"
+  RFQDueDate?: Date; //Column "RFQ Due Date"
+  OrderType?: string; //Column "Order Type"
+  RFQInstructionToSupplier?: string; //Column "RFQ Instruction To Supplier"
+  RFQStatus?: string; //Column "RFQ Status"
+  BuyerInfo?: string; //Column "Buyer Info"
+  SectionInfo?: string; //Column "Section Info"
+  CommentHistory?: string; //Column "Comment History"
+  RequisitionIds?: string; //Column "Requisition Ids"
+  QuoteReceivedDate?: Date; //Column "Quote Received Date"
+  ReasonOfRFQ?: string; //Column "Reason of RFQ"
+  EffectiveDateRequest?: Date; //Column "Effective Date Request"
+  HandlerName?: string; //Column "Handler Name"
+  RFQNo?: string; //Column "RFQ No."
+  Created?: Date; //SharePoint Item Create Date
+  RFQType?: string; //Column "RFQ Type"
 }
 export interface IRFQRequisition {
   RFQ: IRFQGrid;
